1b31422bafb9022f407a3790fcf0a15dc0696312
Fabricio Dos Santos <fabricioperazzo@gmail.com>
2022-09-07 12:11:49 -0400

    Added functionnality to the number buttons. Numbers are displayed in the result up to 8 digits

--- calculator.js
+++ calculator.js
@@ -8,3 +8,27 @@ function divide(x, y) { return x / y; }
 function operate(operator, x, y) {
     return operator(x, y);
 }
+
+const resultDisplay = document.querySelector(".result");
+
+const allButtonsArray = Array.from(document.querySelectorAll(".but"));
+const numButtons = allButtonsArray.filter((button) => {return !isNaN(button.id);}).sort((b1, b2) => {return (parseInt(b1.id) - parseInt(b2.id));});
+const operationsButtons =  allButtonsArray.filter((button) => {return isNaN(button.id);});
+
+for (let i = 0; i < 10; i++){
+    let numButton = numButtons[i];
+    numButton.addEventListener("click", (event) => {
+        let currentText = resultDisplay.textContent;
+        if (currentText.length >= 9) {
+            return;
+        }
+        else if (currentText === "0") {
+            resultDisplay.textContent = `${i}`;
+        }
+        else {
+            resultDisplay.textContent = resultDisplay.textContent + `${i}`;
+        }
+    })
+}
+
+
